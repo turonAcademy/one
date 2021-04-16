a778f7767209ad0480d8c0fb5f60099c64d76245
turonAcademy <73733214+turonAcademy@users.noreply.github.com>
2021-04-16 15:46:25 +0500

    dictionary_and_for
    for in dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,7 @@
 # lesso-14 sariq.dev Dictionary
-car={'moodel':'ferrari','rang':'qizil'}
-print(car)
+# car={'moodel':'ferrari','rang':'qizil'}
+# print(car)
+talaba={'isim':'Xayrullo','kurs':3,'unversitiy':'TATU UF'}
+for key,value in talaba.items():
+    print(f'kalit:{key}')
+    print(f'qiymat:{value}\n')
